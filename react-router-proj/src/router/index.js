392edd1faf7c21ea68d65033744c79fb6a208f9d
nydo(mac) <2337315348@qq.com>
2025-09-22 02:36:42 +1000

    react 两种路由模式

--- react-router-proj/src/router/index.js
+++ react-router-proj/src/router/index.js
@@ -1,12 +1,14 @@
 import Login from "../page/Login";
 import Article from "../page/Article";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createHashRouter } from "react-router-dom";
 import Layout from "../page/Layout";
 import About from "../page/About";
 import Board from "../page/Board";
 import NotFound from "../page/NotFound";
 
 const router = createBrowserRouter([
+  // 这个是 hash 路由模式
+  // const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
