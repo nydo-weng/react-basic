7827559eefcfe3703c2dbb9cbb880a662dff04c8
nydo(mac) <2337315348@qq.com>
2025-09-22 01:53:08 +1000

    TAT BYBIT 前端实习 面试 被拒了

--- react-router-proj/src/page/Article/index.js
+++ react-router-proj/src/page/Article/index.js
@@ -1,5 +1,14 @@
+// import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 const Article = () => {
-  return <div>我是文章页</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      我是文章页
+      <button onClick={() => navigate("/login")}>跳转登录页</button>
+    </div>
+  );
 };
 
 export default Article;
--- react-router-proj/src/page/Login/index.js
+++ react-router-proj/src/page/Login/index.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>我是登录页</div>;
+  return (
+    <div>
+      {/* 这是使用 Link 组件进行跳转, 声明式的导航 */}
+      我是登录页<Link to="/article">跳转文章页</Link>
+    </div>
+  );
 };
 
 export default Login;
