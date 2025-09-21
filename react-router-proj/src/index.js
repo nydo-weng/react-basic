8702dfc53c0b3aa9b3a5406df8fc41fc3581e7ba
nydo(mac) <2337315348@qq.com>
2025-09-21 22:45:18 +1000

    优化路由, 把页面级别(路由级别)的组件放到 page 下, 在 router 下配置路由 router, 在入口导入 router 然后绑定

--- react-router-proj/src/index.js
+++ react-router-proj/src/index.js
@@ -6,22 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-// 1. 创建 router 实例对象, 并且配置路由对应关系
-
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <div>我是登录页</div>,
-  },
-  {
-    path: "/article",
-    element: <div>我是文章页</div>,
-  },
-]);
+// 1. 导入路由 router
+import router from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/*  2. 路由绑定 */}
     <RouterProvider router={router}> </RouterProvider>
   </React.StrictMode>
 );
