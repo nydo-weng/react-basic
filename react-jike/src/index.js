cfc7b73788e91fb34a97480e336517da5953c2bb
nydo(mac) <2337315348@qq.com>
2025-09-29 03:04:15 +1000

    jike: 利用 Normalize.css 重置样式, 并且通过设置 html, body, #root 的 height 为100% 来让组件撑满整个屏幕

--- react-jike/src/index.js
+++ react-jike/src/index.js
@@ -8,6 +8,9 @@ import router from "./router";
 import { Provider } from "react-redux";
 import store from "./store";
 
+// 初始化样式
+import "normalize.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
