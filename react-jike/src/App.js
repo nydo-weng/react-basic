14eaa4584be24e225144d80eba921238efde15e7
nydo(mac) <2337315348@qq.com>
2025-09-28 22:30:29 +1000

    jike: 前期准备 + login 静态页面

--- react-jike/src/App.js
+++ react-jike/src/App.js
@@ -1,5 +1,11 @@
+import { Button } from "antd";
+
 function App() {
-  return <div>this is App</div>;
+  return (
+    <div>
+      this is App <Button type="primary">test</Button>
+    </div>
+  );
 }
 
 export default App;
