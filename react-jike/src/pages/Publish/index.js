9528c3e44b23c9b0a67c806b74ead15a0edac947
nydo(mac) <2337315348@qq.com>
2025-09-29 17:22:43 +1000

    jike: publish 基础静态结构

--- react-jike/src/pages/Publish/index.js
+++ react-jike/src/pages/Publish/index.js
@@ -1,5 +1,59 @@
+import { Card, Breadcrumb, Form, Button, Radio, Input, Upload, Space, Select } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import "./index.scss";
+
+const { Option } = Select;
+
 const Publish = () => {
-  return <div>this is publish</div>;
+  return (
+    <div className="publish">
+      <Card
+        title={
+          <Breadcrumb separator=">">
+            <Breadcrumb.Item>
+              <Link to="/home">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>发布文章</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+      >
+        <Form labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 1 }}>
+          <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: "请输入文章标题" }]}
+          >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item
+            label="频道"
+            name="channel_id"
+            rules={[{ required: true, message: "请选择文章频道" }]}
+          >
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+              <Option value={0}>推荐</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="内容"
+            name="content"
+            rules={[{ required: true, message: "请输入文章内容" }]}
+          >
+            {/* 放一个富文本编辑器 */}
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size="large" type="primary" htmlType="submit">
+                发布文章
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Publish;
