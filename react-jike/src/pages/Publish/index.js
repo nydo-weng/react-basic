c2e7e7d6e319ad36d883dccb6a5c790dfeb8f47e
nydo(mac) <2337315348@qq.com>
2025-09-30 03:30:18 +1000

    jike: 根据是否有 id 调整导航显示, 并且只在有 id 时获取详情

--- react-jike/src/pages/Publish/index.js
+++ react-jike/src/pages/Publish/index.js
@@ -60,7 +60,8 @@ const Publish = () => {
   useEffect(() => {
     // 1. 通过 id 获取数据
     async function getArticleDetail() {
-      if (!articleId) return;
+      // 直接到调用函数前面去判断
+      // if (!articleId) return;
       const res = await getArticleByIdAPI(articleId);
       const data = res.data;
       const { cover } = data;
@@ -79,7 +80,8 @@ const Publish = () => {
         }),
       );
     }
-
+    // 只有有 id 时才回填
+    if (!articleId) return;
     getArticleDetail();
     // 2. 调用实例方法 完成回填
   }, [articleId, form]);
@@ -89,7 +91,10 @@ const Publish = () => {
         title={
           <Breadcrumb
             separator="/"
-            items={[{ title: <Link to="/">首页</Link> }, { title: "发布文章" }]}
+            items={[
+              { title: <Link to="/">首页</Link> },
+              { title: `${articleId ? "编辑" : "发布"}文章` },
+            ]}
           />
         }
       >
