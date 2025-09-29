da4666274021acd7ea6c7e1ffb83c520b727cb5e
nydo(mac) <2337315348@qq.com>
2025-09-29 14:55:29 +1000

    jike: 获取用户信息, 存到 redux store 中

--- react-jike/src/pages/Layout/index.js
+++ react-jike/src/pages/Layout/index.js
@@ -4,6 +4,11 @@ import "./index.scss";
 
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserInfo } from "@/store/modules/user";
+
+import { useEffect } from "react";
+
 const { Header, Sider } = Layout;
 
 const items = [
@@ -37,12 +42,19 @@ const GeekLayout = () => {
   const selectedkey = location.pathname;
   // console.log(location.pathname);
 
+  // 触发个人信息获取 action
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUserInfo());
+  }, [dispatch]);
+
+  const name = useSelector((state) => state.user.userInfo.name);
   return (
     <Layout>
       <Header className="header">
         <div className="logo" />
         <div className="user-info">
-          <span className="user-name">柴柴老师</span>
+          <span className="user-name">{name}</span>
           <span className="user-logout">
             <Popconfirm title="是否确认退出? " okText="退出" cancelText="取消">
               <LogoutOutlined /> 退出
--- react-jike/src/store/modules/user.js
+++ react-jike/src/store/modules/user.js
@@ -10,6 +10,7 @@ const userStore = createSlice({
   // 数据状态
   initialState: {
     token: getToken() || "",
+    userInfo: {},
   },
   // 同步修改方法
   reducers: {
@@ -18,14 +19,14 @@ const userStore = createSlice({
       // 在 localstorage 也存一份
       _setToken(action.payload);
     },
+    setUserInfo(state, action) {
+      state.userInfo = action.payload;
+    },
   },
 });
 
 // 解构出 actionCreater
-const { setToken } = userStore.actions;
-
-// 获取 reducer 函数
-const userReducer = userStore.reducer;
+const { setToken, setUserInfo } = userStore.actions;
 
 // 异步方法 完成登录 获取 token
 const fetchLogin = (loginForm) => {
@@ -37,6 +38,18 @@ const fetchLogin = (loginForm) => {
   };
 };
 
-export { setToken, fetchLogin };
+// 获取个人信息的异步方法
+const fetchUserInfo = () => {
+  return async (dispatch) => {
+    const res = await request.get("/user/profile");
+    dispatch(setUserInfo(res.data));
+  };
+};
+
+// export { setToken, fetchLogin, fetchUserInfo };
+export { fetchLogin, fetchUserInfo };
+
+// 获取 reducer 函数
+const userReducer = userStore.reducer;
 
 export default userReducer;
