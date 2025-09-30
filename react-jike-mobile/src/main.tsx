1d695300f1874d269a656b9ecb55e80300901c96
nydo(mac) <2337315348@qq.com>
2025-10-01 02:54:32 +1000

    jike-mobile: 基础路由配置

--- react-jike-mobile/src/main.tsx
+++ react-jike-mobile/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
+// 导入 router 实例
+import router from "./router/index.tsx";
+// 导入 provider
+import { RouterProvider } from "react-router-dom";
 
-import App from "./App.tsx";
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
