8d5439e8920c1c60be0b7618c394cf9076f082c1
nydo(mac) <2337315348@qq.com>
2025-10-01 01:14:29 +1000

    react-ts-vite: useState 配合泛型, 可以限制很多类型相关的语法

--- react-ts-vite/src/App.tsx
+++ react-ts-vite/src/App.tsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 // useState 根据初始值做自动推断
 // 场景: 明确的初始值
 
+type User = {
+  name: string;
+  age: number;
+};
+
 function App() {
   const [value, toggle] = useState(false);
 
@@ -15,9 +20,47 @@ function App() {
   const changeList = () => {
     setList([4, 5, 6]);
   };
+
+  // 1. 限制初始值类型
+  // const [user, setUser] = useState<User>({
+  //   name: "jack",
+  //   age: 18,
+  // });
+
+  // const [user, setUser] = useState<User>(() => {
+  //   return {
+  //     name: "jack",
+  //     age: 18,
+  //   };
+  // });
+
+  const [user, setUser] = useState<User>({
+    name: "jack",
+    age: 19,
+  });
+
+  // const changeUser = () => {
+  //   setUser(() => ({
+  //     name: "john",
+  //     age: 28,
+  //   }));
+  // };
+
+  // 当初始值置空的时候, setUser 可以接受 undefined, 但是使用情况很少
+  // const changeUser = () => {
+  //   setUser(undefined);
+  // };
+
+  const changeUser = () => {
+    setUser({
+      name: "john",
+      age: 28,
+    });
+  };
+
   return (
     <>
-      this is app. {value}, {list}
+      this is app. {value}, {list}, {user.name}
     </>
   );
 }
