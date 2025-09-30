4ceb4ff1995d2814c4a5a5b2aede837365bc5bc9
nydo(mac) <2337315348@qq.com>
2025-09-30 17:20:16 +1000

    jike: CDN配置, 失败, 下次继续看

--- react-jike/craco.config.js
+++ react-jike/craco.config.js
@@ -1,7 +1,10 @@
 // 扩展 webpack 的配置
 
 const path = require("path");
+// 引入辅助函数
+const { whenProd, getPlugin, pluginByName } = require("@craco/craco");
 
+console.log("Current NODE_ENV:", process.env.NODE_ENV);
 module.exports = {
   // webpack 配置
   webpack: {
@@ -9,6 +12,38 @@ module.exports = {
     alias: {
       // 约定: 使用 @ 表示 src 文件所在路劲
       "@": path.resolve(__dirname, "src"),
+    },
+    // 配置 CDN
+    configure: (webpackConfig) => {
+      let cdn = { js: [], css: [] }; // 添加默认值
+      whenProd(() => {
+        console.log("进生产");
+        webpackConfig.externals = {
+          react: "React",
+          "react-dom": "ReactDOM",
+        };
+        cdn = {
+          js: [
+            "https://cdnjs.cloudflare.com/ajax/libs/react/18.1.0/umd/react.production.min.js",
+            "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.1.0/umd/react-dom.production.min.js",
+          ],
+          css: [],
+        };
+      });
+
+      // 通过 htmlWebpackPlugin 插件, 在 public/index.html注入 cdn 资源 url
+      const { isFound, match } = getPlugin(webpackConfig, pluginByName("HtmlWebpackPlugin"));
+
+      if (isFound) {
+        // 找到了 HtmlWebpackPlugin 的插件
+        console.log("HtmlWebpackPlugin found");
+        console.log("CDN configuration:", cdn);
+        match.userOptions.cdn = cdn;
+      } else {
+        console.log("HtmlWebpackPlugin not found");
+      }
+
+      return webpackConfig;
     },
   },
 };
