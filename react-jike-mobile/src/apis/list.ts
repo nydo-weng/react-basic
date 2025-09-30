94d7d229636fc02f63ba802f716931399d7ce368
nydo(mac) <2337315348@qq.com>
2025-10-01 03:27:06 +1000

    jike-mobile: apis 配合 ts 进行封装

---
+++ react-jike-mobile/src/apis/list.ts
@@ -0,0 +1,21 @@
+import { http } from "@/utils";
+import type { ResType } from "./shared";
+// 2. 定义具体的接口类型
+type ChannelItem = {
+  id: number;
+  name: string;
+};
+
+type ChannelRes = {
+  channels: ChannelItem[];
+};
+
+// 请求频道列表
+function fetchChannelAPI() {
+  return http.request<ResType<ChannelRes>>({
+    url: "/channels",
+    method: "GET",
+  });
+}
+
+export { fetchChannelAPI };
--- react-jike-mobile/src/main.tsx
+++ react-jike-mobile/src/main.tsx
@@ -4,4 +4,11 @@ import router from "./router/index.tsx";
 // 导入 provider
 import { RouterProvider } from "react-router-dom";
 
+// 测试接口
+import { fetchChannelAPI } from "@/apis/list.ts";
+
+fetchChannelAPI().then((res) => {
+  console.log(res.data.data.channels);
+});
+
 createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
