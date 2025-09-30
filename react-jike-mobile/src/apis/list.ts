d6f793471ebdde27ee00cdb0c889dd58b915bc8d
nydo(mac) <2337315348@qq.com>
2025-10-01 03:46:06 +1000

    jike-mobile: tab 标签栏渲染

--- react-jike-mobile/src/apis/list.ts
+++ react-jike-mobile/src/apis/list.ts
@@ -1,7 +1,7 @@
 import { http } from "@/utils";
 import type { ResType } from "./shared";
 // 2. 定义具体的接口类型
-type ChannelItem = {
+export type ChannelItem = {
   id: number;
   name: string;
 };
--- react-jike-mobile/src/pages/Home/index.tsx
+++ react-jike-mobile/src/pages/Home/index.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+import { Tabs } from "antd-mobile";
+
+import { fetchChannelAPI, type ChannelItem } from "@/apis/list";
+
 const Home = () => {
-  return <div>this is Home</div>;
+  const [channels, setChannels] = useState<ChannelItem[]>([]);
+
+  useEffect(() => {
+    const getChannels = async () => {
+      try {
+        const res = fetchChannelAPI();
+        setChannels((await res).data.data.channels);
+      } catch (error) {
+        throw new Error("fetch channel error");
+      }
+    };
+    getChannels();
+  }, []);
+
+  return (
+    <div>
+      <div className="tabContainer">
+        <Tabs>
+          {channels.map((item) => {
+            return (
+              <Tabs.Tab key={item.id} title={item.name}>
+                {item.name}
+              </Tabs.Tab>
+            );
+          })}
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
