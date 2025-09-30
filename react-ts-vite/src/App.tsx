6332827e0be4b8c30c633bb4fe497ff82d972bf8
nydo(mac) <2337315348@qq.com>
2025-10-01 01:05:54 +1000

    react-ts-vite: useState 在 ts 下, 如果有明确的初始值, 可以根据初始值做自动推断

--- react-ts-vite/src/App.tsx
+++ react-ts-vite/src/App.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
+// useState 根据初始值做自动推断
+// 场景: 明确的初始值
+
 function App() {
-  return <>this is app.</>;
+  const [value, toggle] = useState(false);
+
+  const [list, setList] = useState([1, 2, 3]);
+
+  const changeValue = () => {
+    toggle(true);
+  };
+
+  const changeList = () => {
+    setList([4, 5, 6]);
+  };
+  return (
+    <>
+      this is app. {value}, {list}
+    </>
+  );
 }
 
 export default App;
