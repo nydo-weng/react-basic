675e60286f8afd3b28e8b18b6228866e43c612d2
nydo(mac) <2337315348@qq.com>
2025-09-30 18:16:47 +1000

    react hooks, useReducer 基础使用介绍

---
+++ react-hooks/src/App.js
@@ -0,0 +1,35 @@
+// useReducer
+import { useReducer } from "react";
+
+// 1. 定义一个 reducer 函数, 根据不同的 action 返回不同的状态
+function reducer(state, action) {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "SET":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+// 2. 组件中调用 useReducer(reduer, 0(状态初始值)) => [state, dispatch]
+
+// 3. 调用 dispatch({type: "INC"}) => 通知 reducer 产生一个新的状态, 使用这个新状态更新 UI
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <div className="App">
+      this is app {state}
+      <button onClick={() => dispatch({ type: "INC" })}>+</button>
+      <button onClick={() => dispatch({ type: "DEC" })}>-</button>
+      <button onClick={() => dispatch({ type: "SET", payload: "100" })}>UPDATE</button>
+    </div>
+  );
+}
+
+export default App;
